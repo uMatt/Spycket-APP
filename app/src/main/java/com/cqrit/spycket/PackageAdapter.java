7dfdf3691950b6e9be7a051c925217e8c8cb21d3
uMatt <34341721+UMATT@users.noreply.github.com>
2024-03-02 18:06:51 +0100

    Add Analysis Activity

--- app/src/main/java/com/cqrit/spycket/PackageAdapter.java
+++ app/src/main/java/com/cqrit/spycket/PackageAdapter.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 public class PackageAdapter extends ArrayAdapter<String> {
     private List<String> packages;
+    p
 
     public PackageAdapter(Context context, List<String> packages) {
         super(context, 0, packages);
