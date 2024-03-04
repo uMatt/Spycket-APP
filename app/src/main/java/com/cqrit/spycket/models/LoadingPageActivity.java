05035441234471a711deb263b4f7dac65f40713e
Buuuntyyy <104865101+Buuuntyyy@users.noreply.github.com>
2024-03-04 11:22:09 +0100

    Update welcome page

---
+++ app/src/main/java/com/cqrit/spycket/models/LoadingPageActivity.java
@@ -0,0 +1,44 @@
+package com.cqrit.spycket.models;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.cqrit.spycket.MainActivity;
+import com.cqrit.spycket.R;
+
+public class LoadingPageActivity extends AppCompatActivity {
+
+    private final int LOADING_ACTIVITY_TIMEOUT = 3000;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_loading_page);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, LOADING_ACTIVITY_TIMEOUT);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+
+        });
+    }
+}
